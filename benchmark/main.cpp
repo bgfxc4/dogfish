d4506970d2c3fa5cba54dd1cefbe6c33eb141692
bgfxc4 <bindermarc@gmx.de>
2021-04-08 01:01:38 +0200

    added small benchmark

---
+++ benchmark/main.cpp
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <iostream>
+
+#include "board.hpp"
+#include "fossile_chess.hpp"
+
+int main() {
+	Board board;
+	FossileChess engine;
+
+	Move m(0, 6, 0, 5);
+
+	auto t1 = std::chrono::high_resolution_clock::now();
+
+	board.move(m);
+	Move em = engine.get_best_move(&board, 4, 1);
+	
+	auto t2 = std::chrono::high_resolution_clock::now();
+	
+	auto time = duration_cast<std::chrono::milliseconds>(t2 - t1);
+	
+	std::cout << "Found move: " << em.from_x << " " << em.from_y << " to " << em.to_x << " " << em.to_y << ", time used: " << time.count() << std::endl;
+	return 0;
+}
