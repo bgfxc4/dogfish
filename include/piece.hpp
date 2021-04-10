4b06dba130a610d5718d6111357f1796c6d275bf
Lars Lenckowski <larslenckowski@gmail.com>
2021-04-10 15:22:48 +0200

    make inlining the piece constructor easier

--- include/piece.hpp
+++ include/piece.hpp
@@ -12,7 +12,8 @@ public:
 	uint8_t type : 7;
 	uint8_t is_white : 1;
 
-	Piece(uint8_t _type, uint8_t _is_white);
+	// is_white must be 0 or 1
+	Piece(uint8_t _type, uint8_t _is_white) : type(_type), is_white(_is_white) {}
 
 	template<typename A, typename B>
 	Piece(A _type, B _is_white) : Piece((uint8_t)_type, (uint8_t)_is_white) {}
--- lib/piece.cpp
+++ lib/piece.cpp
@@ -4,12 +4,6 @@
 #include "board.hpp"
 #include "constants.hpp"
 
-// _is_white has to be 0 or 1
-Piece::Piece(uint8_t _type, uint8_t _is_white) {
-	type = _type;
-	is_white = _is_white;
-}
-
 static void add_rook_moves(Board& board, int x, int y,
 		std::vector<Move>& res)
 {
